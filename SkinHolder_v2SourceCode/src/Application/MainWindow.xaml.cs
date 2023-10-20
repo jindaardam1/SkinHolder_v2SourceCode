f4abb47e6501ae4fc87a7d9c7148d870405317ff
Jagoba Inda <jindaardam1@gmail.com>
2023-10-20 04:45:07 +0200

    Salir con Esc

--- SkinHolder_v2SourceCode/src/Application/MainWindow.xaml.cs
+++ SkinHolder_v2SourceCode/src/Application/MainWindow.xaml.cs
@@ -1,43 +1,49 @@
 ﻿using System;
-using System.Threading;
 using System.Windows;
-using SkinHolder_v2SourceCode.Utils;
+using System.Windows.Input;
 using SkinHolder_v2SourceCode.Views;
 
-namespace SkinHolder_v2SourceCode.Application
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            MostrarSplashScreen();
-        }
-        
-        private void MostrarSplashScreen()
-        {
-            var pi = new PantallaInicio();
-            MainContent.Content = pi;
+namespace SkinHolder_v2SourceCode.Application;
 
-            var timer = new System.Windows.Threading.DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(1.2)
-            };
-            timer.Tick += (sender, e) =>
-            {
-                timer.Stop();
-                MostrarMenuPrincipal();
-            };
-            timer.Start();
-        }
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MostrarSplashScreen();
+    }
         
-        private void MostrarMenuPrincipal()
+    private void MostrarSplashScreen()
+    {
+        var pi = new PantallaInicio();
+        MainContent.Content = pi;
+
+        var timer = new System.Windows.Threading.DispatcherTimer
         {
-            var menuPrincipalView = new MenuPrincipal();
-            MainContent.Content = menuPrincipalView;
+            Interval = TimeSpan.FromSeconds(1.2)
+        };
+        timer.Tick += (sender, e) =>
+        {
+            timer.Stop();
+            MostrarMenuPrincipal();
+        };
+        timer.Start();
+    }
+        
+    private void MostrarMenuPrincipal()
+    {
+        var menuPrincipalView = new MenuPrincipal();
+        MainContent.Content = menuPrincipalView;
+    }
+    
+    private void CerrarConEscape(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
--- SkinHolder_v2SourceCode/src/Views/MenuPrincipal.xaml.cs
+++ SkinHolder_v2SourceCode/src/Views/MenuPrincipal.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SkinHolder_v2SourceCode.Views;
 
@@ -7,5 +10,18 @@ public partial class MenuPrincipal : UserControl
     public MenuPrincipal()
     {
         InitializeComponent();
+    }
+    
+    private void Grid_Loaded(object sender, RoutedEventArgs e)
+    {
+        var grid = (Grid)sender;
+        
+        var window = Window.GetWindow(grid);
+
+        if (window != null)
+        {
+            grid.Width = window.ActualWidth / 1.3;
+            grid.Height = window.ActualHeight / 1.3;
+        }
     }
 }
